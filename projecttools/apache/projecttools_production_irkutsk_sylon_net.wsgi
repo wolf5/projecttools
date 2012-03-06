16edd790a2f46518bb6d5c006e836a3bd3ace6e5
Timo Würsch <twuersch@gmail.com>
2012-03-06 14:17:16 +0100

    Modified paths to suit production environment.

--- projecttools/apache/projecttools_production_irkutsk_sylon_net.wsgi
+++ projecttools/apache/projecttools_production_irkutsk_sylon_net.wsgi
@@ -5,7 +5,7 @@ path = "/var/www/projecttools"
 if path not in sys.path:
     sys.path.append(path)
 
-os.environ['DJANGO_SETTINGS_MODULE'] = 'projecttools.settings'
+os.environ['DJANGO_SETTINGS_MODULE'] = 'projecttools.settings_production_irkutsk_sylon_net'
 
 import django.core.handlers.wsgi
 application = django.core.handlers.wsgi.WSGIHandler()
--- projecttools/settings_production_irkutsk_sylon_net.py
+++ projecttools/settings_production_irkutsk_sylon_net.py
@@ -1,3 +1,21 @@
 from settings import *
 
-DEBUG = TEMPLATE_DEBUG = False
+DEBUG = False
+TEMPLATE_DEBUG = False
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': '/var/www/projecttools/projecttools/sqlite.db',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    '/var/www/projecttools/projecttools/templates'
+)
