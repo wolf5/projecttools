a38293054ebb8b9ae3ec0f16114e82371bb228d0
Timo Würsch <twuersch@gmail.com>
2012-03-13 14:59:12 +0100

    Renamed database to follow general conventions (.sqlite extension).

--- projecttools/settings_production_irkutsk_sylon_net.py
+++ projecttools/settings_production_irkutsk_sylon_net.py
@@ -6,7 +6,7 @@ from settings import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/var/www/projecttools/projecttools/sqlite.db',                      # Or path to database file if using sqlite3.
+        'NAME': '/var/www/projecttools/projecttools/database.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
