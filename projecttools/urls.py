5b10b4f909531006689ee899df57a5086dd7e9de
Timo Würsch <twuersch@gmail.com>
2012-03-14 14:27:20 +0100

    Logout link; Admin link; Copyright info

--- projecttools/urls.py
+++ projecttools/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -9,5 +10,6 @@ urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r"^$", "timesheet.views.clock"),
     url(r"^login/", "django.contrib.auth.views.login", {"template_name": "timesheet/login.html"}),
+    url(r"^logout/", "django.contrib.auth.views.logout_then_login", {"login_url": settings.LOGIN_URL}),
     url(r"^customer/(?P<customer_id>\d+)/report(\.(?P<format_identifier>\w+))?/((?P<year>\d{4})(/(?P<month>\d{1,2}))?)?", "timesheet.views.customer_report")
 )
