6e1148f195ff4a13b70e50fd0164b7e65b32caa1
Timo Würsch <twuersch@gmail.com>
2012-03-14 13:26:03 +0100

    In the admin, everyone can only see their own entries (except for superusers)

--- projecttools/timesheet/admin.py
+++ projecttools/timesheet/admin.py
@@ -2,5 +2,15 @@ from models import Customer
 from models import Entry
 from django.contrib import admin
 
+class OwnEntryAdmin(admin.ModelAdmin):
+    def queryset(self, request):
+        originalQuerySet = super(OwnEntryAdmin, self).queryset(request)
+        
+        if request.user.is_superuser:
+            return originalQuerySet
+        else:
+            filteredQuerySet = originalQuerySet.filter(owner = request.user)
+            return filteredQuerySet
+
 admin.site.register(Customer)
-admin.site.register(Entry)
+admin.site.register(Entry, OwnEntryAdmin)
