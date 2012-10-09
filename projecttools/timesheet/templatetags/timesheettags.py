a0bed66f81bfdbf23345a2eea8fbb6da9ddcbc71
Timo Würsch <twuersch@gmail.com>
2012-10-09 23:11:06 +0200

    Rundung in duration. Korrigiert #3.

--- projecttools/timesheet/templatetags/timesheettags.py
+++ projecttools/timesheet/templatetags/timesheettags.py
@@ -21,24 +21,66 @@ def duration(value, arg = "%h:%m"):
     h -- hours, 0...23
     H -- hours, integer, values > 23 possible
     o -- hours, 2 decimals, 0...23.99
-    O -- hours, 2 decimals, values > 23 possible
+    O -- hours, 2 decimals, values > 24 possible
     D -- days, integer
     """
     
     # Fail silently if the value is not a timedelta.
     if not isinstance(value, timedelta):
         return ""
+
+    # Pass 1: Determine which is the smallest unit we have to display
+    awaitingFormatIdentifier = False
+    seconds_smallest_unit = minutes_smallest_unit = hours_smallest_unit = days_smallest_unit = False
+    for c in arg:
+        if c == "%":
+            if awaitingFormatIdentifier:
+                awaitingFormatIdentifier = False
+            else:
+                awaitingFormatIdentifier = True
+        else:
+            if awaitingFormatIdentifier:
+                if c == "s"or c == "S":
+                    seconds_smallest_unit = True
+                    minutes_smallest_unit = hours_smallest_unit = days_smallest_unit = False
+                elif c == "m" or c == "M":
+                    minutes_smallest_unit = not seconds_smallest_unit
+                    hours_smallest_unit = days_smallest_unit = False
+                elif c == "h" or c == "H" or c == "o" or c == "O":
+                    hours_smallest_unit = not minutes_smallest_unit and not seconds_smallest_unit
+                    days_smallest_unit = False
+                elif c == "D":
+                    days_smallest_unit = not hours_smallest_unit and not minutes_smallest_unit and not seconds_smallest_unit
+                else:
+                    pass
+                awaitingFormatIdentifier = False
+            else:
+                pass
     
+    # Now, dissect value and add rounding where necessary
     totalSeconds = (value.microseconds + (value.seconds + value.days * 24 * 3600) * 10**6) / float(10**6)
+    
+    unroundedTotalDays, remainder = divmod(totalSeconds, 86400)
+    unroundedModHours, remainder = divmod(remainder, 3600)
+    unroundedModMinutes, unroundedModSeconds = divmod(remainder, 60)
+    unroundedTotalHours = math.floor(totalSeconds / 3600)
+    unroundedTotalMinutes = math.floor(totalSeconds / 60)
+    
+    if days_smallest_unit and divmod(totalSeconds, 86400)[1] >= 43200:
+        totalSeconds = totalSeconds + 86400
+    elif hours_smallest_unit and divmod(totalSeconds, 3600)[1] >= 1800:
+        totalSeconds = totalSeconds + 3600
+    elif minutes_smallest_unit and divmod(totalSeconds, 60)[1] >= 30:
+        totalSeconds = totalSeconds + 60
     totalDays, remainder = divmod(totalSeconds, 86400)
     modHours, remainder = divmod(remainder, 3600)
     modMinutes, modSeconds = divmod(remainder, 60)
     totalHours = math.floor(totalSeconds / 3600)
     totalMinutes = math.floor(totalSeconds / 60)
-
+    
+    # Pass 2: Assemble the output
     niceOutput = ""
     awaitingFormatIdentifier = False
-    
     for c in arg:
         if c == "%":
             if awaitingFormatIdentifier:
@@ -61,10 +103,10 @@ def duration(value, arg = "%h:%m"):
                 elif c == "H":
                     niceOutput = niceOutput + str(int(totalHours))
                 elif c == "o":
-                    decimalModHours = modHours + (modMinutes / 60)
+                    decimalModHours = unroundedModHours + (unroundedModMinutes / 60)
                     niceOutput = niceOutput + ("%.2f" % decimalModHours)
                 elif c == "O":
-                    decimalTotalHours = totalHours + (modMinutes / 60)
+                    decimalTotalHours = unroundedTotalHours + (unroundedModMinutes / 60)
                     niceOutput = niceOutput + ("%.2f" % decimalTotalHours)
                 elif c == "D":
                     niceOutput = niceOutput + str(int(totalDays))
